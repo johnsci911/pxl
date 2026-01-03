a39361ab47aed5ee165218a776f8fc44fca14d22
jkcachero <johnkarlo.315@gmail.com>
2026-01-04 03:15:30 +0800

    Profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Profile extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'display_name',
+        'handle',
+        'bio',
+        'avatar_url',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
